32b9607179c4112a565fb80c4971e783ff355ad3
Abhishek banyal <abhishekbanyal35@gmail.com>
2026-03-24 08:07:52 +0530

    final code

--- Website/vite.config.js
+++ Website/vite.config.js
@@ -4,6 +4,6 @@ import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: process.env.VITE_BASE_URL || "/E-commerce/Website",
+  base: process.env.VITE_BASE_URL || "/E-commerce",
 }
 );
